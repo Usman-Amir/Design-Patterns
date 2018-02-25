8ff26e93f76ff0a5f81e8b34a459f07bfea6ea0e
Usman-Amir <usman.amir@live.com>
2018-02-25 21:49:46 +0800

    Singleton Design Pattern along with UnitTest using NUnit

---
+++ SingletonTest/SingletonTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+
+using NUnitAssert = NUnit.Framework.Assert;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Singleton;
+
+namespace SingletonTest
+{
+	[TestClass]
+	public class SingletonTest
+	{
+		[TestMethod]
+		public void CreateInstance_TwoTime_OnlyOneInstanceShouldbeCreated()
+		{
+			//Arrange
+			MarioGame player1;
+			MarioGame player2;
+			//Act
+			player1 = MarioGame.GetInstance();
+			player2 = MarioGame.GetInstance();
+			//Assert
+			Assert.AreEqual(player1, player2, "Both the instances are equal");
+		}
+	}
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Singleton;
+
+namespace Test
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			MarioGame mg = MarioGame.GetInstance();
+			Assert.AreEqual("", "");
+		}
+	}
+}
